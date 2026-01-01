f2d6c3f046a72f5675ec38a18f529b7438c499b8
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-01-01 20:45:13 +0700

    feat: set up Express server and add EJS view engine

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import type { Request, Response } from 'express'
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('./public'))
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Aplikasi berjalan di: http://localhost:${port}`)
+})
