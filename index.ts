39698ffa20f05aa0df148a4a506c269f08f6e5c3
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-01-01 21:06:41 +0700

    feat: add Post interface and sample posts for rendering on the homepage

--- index.ts
+++ index.ts
@@ -7,8 +7,23 @@ const port = 3000
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
+interface Post {
+  id: string
+  title: string
+  content: string
+}
+
+const posts: Post[] = [
+  {
+    id: crypto.randomUUID.toString(),
+    title: 'Apa Itu Emotional Quotient?',
+    content:
+      'Emotional Quotient (EQ) atau Kecerdasan Emosional adalah kemampuan seseorang untuk mengenali, memahami, mengelola, dan mengekspresikan emosi diri sendiri serta orang lain secara tepat, serta menggunakan emosi tersebut untuk membangun hubungan sosial yang baik, memotivasi diri, dan mengambil keputusan yang bijaksana, seringkali dianggap lebih penting untuk kesuksesan hidup dibandingkan IQ. ',
+  },
+]
+
 app.get('/', (req: Request, res: Response) => {
-  res.render('index')
+  res.render('index', { posts })
 })
 
 app.listen(port, () => {
